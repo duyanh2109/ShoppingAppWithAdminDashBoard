92c15aa8b3097d8fd75b2e3a79e9e2c9c8e1f966
unknown <anhnd.yrc@gmail.com>
2023-12-08 12:12:27 +0700

    reversed

--- my-app/app/api/[storeId]/checkout/route.ts
+++ my-app/app/api/[storeId]/checkout/route.ts
@@ -9,6 +9,10 @@ const corsHeaders = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
@@ -62,5 +66,5 @@ export async function POST(
     },
   });
   console.log(session);
-  return NextResponse.json({ url: session.url });
+  return NextResponse.json({ url: session.url }, { headers: corsHeaders });
 }
