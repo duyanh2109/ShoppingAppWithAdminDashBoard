d3696d637b436b6647f527651be83c9a8f39d45e
unknown <anhnd.yrc@gmail.com>
2023-12-08 11:03:37 +0700

    add headers to next config to see if they can fix cors problem

--- my-app/app/api/[storeId]/checkout/route.ts
+++ my-app/app/api/[storeId]/checkout/route.ts
@@ -10,7 +10,7 @@ const corsHeaders = {
 };
 
 export async function OPTIONS() {
-  return NextResponse.json({}, { headers: corsHeaders });
+  return NextResponse.json(null, { headers: corsHeaders });
 }
 
 export async function POST(
