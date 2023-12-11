728b3ed34a8a68cae42570cd0d0e2b5976cea784
unknown <anhnd.yrc@gmail.com>
2023-12-11 13:16:41 +0700

    add Revenue Graph

--- my-app/app/(dashboard)/[storeId]/(routes)/page.tsx
+++ my-app/app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -7,6 +7,8 @@ import { formatter } from "@/lib/utils";
 import { getTotalRevenue } from "@/actions/getTotalRevenue";
 import { getSalesCount } from "@/actions/getSalesCount";
 import { getStockCount } from "@/actions/getStockCount";
+import Overview from "@/components/Overview";
+import { getGraphRevenue } from "@/actions/getGraphRevenue";
 interface DashBoardPageProps {
   params: { storeId: string };
 }
@@ -14,6 +16,7 @@ const DashBoardPage: React.FC<DashBoardPageProps> = async ({ params }) => {
   const totalRevenue = await getTotalRevenue(params.storeId);
   const salesCount = await getSalesCount(params.storeId);
   const stockCount = await getStockCount(params.storeId);
+  const graphRevenue = await getGraphRevenue(params.storeId)
   return (
     <div className="flex-col ">
       <div className="flex-1 space-y-4 p-8 pt-6">
@@ -57,6 +60,14 @@ const DashBoardPage: React.FC<DashBoardPageProps> = async ({ params }) => {
             </CardContent>
           </Card>
         </div>
+        <Card className="col-span-4">
+          <CardHeader>
+            <CardTitle> Overview</CardTitle>
+          </CardHeader>
+          <CardContent className="pl-2">
+            <Overview data={graphRevenue} />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
