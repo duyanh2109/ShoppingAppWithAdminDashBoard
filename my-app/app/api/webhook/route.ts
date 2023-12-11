c7f95d825568e9a76b89148385e04fd2323c199b
unknown <anhnd.yrc@gmail.com>
2023-12-11 08:28:40 +0700

    back to req.text() instead of using micro to buffer

--- my-app/app/api/webhook/route.ts
+++ my-app/app/api/webhook/route.ts
@@ -1,26 +1,23 @@
-import { NextResponse } from "next/server";
+import { headers } from "next/headers";
 import Stripe from "stripe";
 import { stripe } from "@/lib/stripe";
-import { buffer } from "micro";
+import { NextResponse } from "next/server";
 import prismadb from "@/lib/prisma-db";
 
-export async function POST(req: any) {
-  const buf = await buffer(req);
-  const signature = req.headers["stripe-signature"] as string;
+export async function POST(req: Request) {
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string
   let event: Stripe.Event;
 
   try {
     event = stripe.webhooks.constructEvent(
-      buf.toString(),
+      body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET!
     );
   } catch (error: any) {
-    return new NextResponse("Webhook Error: " + error.message, {
-      status: 400,
-    });
+    return new NextResponse("Webhook Error: " + error.message, { status: 400 });
   }
-
   const session = event.data.object as Stripe.Checkout.Session;
   const address = session?.customer_details?.address;
 
@@ -33,40 +30,33 @@ export async function POST(req: any) {
     address?.country,
   ];
   const addressString = addressComponent.filter((c) => c !== null).join(", ");
+  if(event.type === "checkout.session.completed"){
+    const order = await prismadb.order.update({
+        where:{
+            id: session?.metadata?.orderId
+        },
+        data:{
+            isPaid:true,
+            address:addressString,
+            phone: session?.customer_details?.phone||'',
 
-  if (event.type === "checkout.session.completed") {
-    try {
-      const order = await prismadb.order.update({
-        where: {
-          id: session?.metadata?.orderId,
         },
-        data: {
-          isPaid: true,
-          address: addressString,
-          phone: session?.customer_details?.phone || "",
-        },
-        include: {
-          orderItems: true,
-        },
-      });
+        include:{
+            orderItems:true,
+        }
+    })
 
-      const productIds = order.orderItems.map((orderItem) => orderItem.productId);
-
-      await prismadb.product.updateMany({
-        where: {
-          id: {
-            in: [...productIds],
-          },
+    const productIds =order.orderItems.map((orderItem) =>orderItem.productId)
+    await prismadb.product.updateMany({
+        where:{
+            id:{
+                in:[...productIds]
+            }
         },
-        data: {
-          isArchived: true,
-        },
-      });
-    } catch (error) {
-      console.error("Error updating order:", error);
-      return new NextResponse("Error updating order", { status: 500 });
-    }
+        data:{
+            isArchived:true
+        }
+    })
   }
-
-  return new NextResponse(null, { status: 200 });
+  return new NextResponse(null,{status:200})
 }
